e4d52e808461f00d11dcd21d49f4da0cec2a33b2
Griggsy23 <chris.griggs@waitawhile.au>
2024-05-07 16:34:07 +0800

    updated main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    character_count = get_character_count(text)
-    print(character_count)
+    characters = get_character_count(text)
+    print(characters)
 
 def get_character_count(text):
     character_count = {}
