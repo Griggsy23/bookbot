cd5f3e4d47b84ea49343867ee7ca222e1c3ea05d
Griggsy23 <chris.griggs@waitawhile.au>
2024-05-08 11:39:19 +0800

    updated main.py with book.dev solution, close but no cigar!

--- main.py
+++ main.py
@@ -2,38 +2,50 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    characters = get_character_count(text)
-    char_list = dict_list(characters)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
     print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
-    print("")
-    print(char_list)
-    
+    print()
 
-def get_character_count(text):
-    character_count = {}
-    for i in text.lower():
-        if i in character_count:
-            character_count[i] += 1
-        else:
-            character_count[i] = 1
-    return character_count       
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+           continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
 
-def dict_list(characters):
-    char_list = []
-    for key, val in characters.items():
-        char_list.append({key, val})
-    return char_list
+    print("--- End report ---")
 
 def get_num_words(text):
     words = text.split()
     return len(words)
-    
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-    
 
 main()
